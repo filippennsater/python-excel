0c27aa74c06592515ec7679acc8b9eb9710622d0
filippennsater <filip.pennsater@hotmail.com>
2023-06-27 23:22:30 +0200

    testing

--- hello.py
+++ hello.py
@@ -13,7 +13,35 @@ wb = load_workbook('hello.xlsx')
 ws = wb.active
 
 #print something from the spreadsheet
+'''
 print("This is the result")
 print(ws["A2"])
+
 print(ws["A2"].value)
+
 print(f'{ws["A2"].value}: {ws["B2"].value}')
+
+name = ws["A2"].value
+
+color = ws["B2"].value
+
+print(f'{name}: {color}')
+
+#grab a a whole column
+
+column_a = ws['A']
+print(column_a)
+
+for cell in column_a:
+    print(f'{cell.value}\n')
+'''
+
+#grab a range
+
+rows = ws['2':'10']
+
+
+
+for x in rows:
+    for y in x:
+        print(y.value)
